41838300df8e949a316ed309de6588d319156bbd
Trifon Trifonov <triffon@gmail.com>
2016-01-07 17:57:25 +0200

    Дефиниции на някои от стандартните функции за списъци.

--- lists.hs
+++ lists.hs
@@ -1,4 +1,7 @@
-import Prelude hiding (elem)
+import Prelude hiding
+  (elem, (++), (!!), null, length,
+   init, last, take, drop, sum, const, ($),
+   map, filter, scanr, scanl)
 
 x :: (Int, String)
 x = (3, "abc")
@@ -11,12 +14,14 @@ type Vector = (Double, Double)
 pow (x,0) = 1
 pow (x,n) = x * pow(x,n-1)
 
-l1 +++ l2 = if null l1 then l2 else head l1 : tail l1 +++ l2
+{-
+l1 ++ l2 = if null l1 then l2
+           else head l1 : tail l1 ++ l2
 
-l !!! n
- | n == 0    = head l
- | otherwise = tail l !!! (n-1)
-
+l !! n
+  | n == 0    = head l
+  | otherwise = tail l !! (n-1)
+-}
 {-
 elem x l
  | null l      = False
@@ -24,4 +29,87 @@ elem x l
  | otherwise   = elem x (tail l)
 -}
 
+{-
 elem x l = not (null l) && (head l == x || elem x (tail l))
+-}
+
+null [] = True
+null _  = False
+
+length []     = 0
+length (_:xs) = 1 + length xs
+
+(++) :: [a] -> [a] -> [a]
+[]     ++ l2 = l2
+(x:xs) ++ l2 = x:xs ++ l2
+
+(!!) :: [a] -> Int -> a
+(x:_)  !! 0 = x
+(_:xs) !! n = xs !! (n-1)
+
+elem :: Eq a => a -> [a] -> Bool
+elem _ []     = False
+elem x (y:ys) = x == y || elem x ys
+
+type Int3 = [(Int,Int,Int)]
+pythagoreanTriple :: Int -> Int3
+pythagoreanTriple n =
+  [ (x, y, z) | x <- [1..n], y <- [1..x-1],
+    z <- [max x y..n], x^2 + y^2 == z^2 ]
+
+init :: [a] -> [a]
+init [x]    = []
+init (x:xs) = x:init xs
+
+last :: [a] -> a
+last [x]    = x
+last (_:xs) = last xs
+
+take :: Int -> [a] -> [a]
+take 0 _= []
+take n (x:xs)
+  | n < 0   = error "Отрицателно число!"
+  | otherwise = x:take (n-1) xs
+
+drop :: Int -> [a] -> [a]
+drop 0 l = l
+drop n (_:xs)
+  | n < 0   = error "Отрицателно число!"
+  | otherwise = drop (n-1) xs
+
+sum :: Num a => [a] -> a
+sum [] = 0
+sum (x:xs) = x + sum xs
+
+const x = \y -> x
+
+-- mult3 = \x -> x * 3
+mult3 = (*3)
+
+drop3 = drop 3
+
+pow3 = \x -> pow (3,x)
+
+pow' _ 0 = 1
+pow' x n = x * pow' x (n-1)
+
+f $ x = f x
+
+map :: (a -> b) -> [a] -> [b]
+map f = foldr (\x -> (f x:)) []
+
+filter p = foldr
+           (\x -> if p x then (x:) else id)
+           []
+
+scanr :: (a -> b -> b) -> b -> [a] -> [b]
+
+{-
+scanr _  nv []     = [nv]
+scanr op nv (x:xs) = x `op` r : rest
+  where rest@(r:_) = scanr op nv xs
+-}
+
+scanr op nv = foldr
+              (\x rest@(r:_) -> x `op` r : rest)
+                [nv]
